77f6b598fc8963aaa2812559bc0691e45d9c8a8c
batres <hbatress@miumg.edu.gt>
2023-10-05 14:54:02 -0600

    cambio de nombre

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { databaseSer } = require('./services/databaseSer');
+const { databaseSer } = require('./servicesBD/databaseSer');
 
 
 const app = express();
