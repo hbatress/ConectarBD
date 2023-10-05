93201d8e44e2628ccdbd7c617bf8b248c0a7edda
batres <hbatress@miumg.edu.gt>
2023-10-05 16:52:35 -0600

    prueba de conexión 1

--- next.js
+++ next.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
 
-const dataToSend = {
-nombre: 'Nombre del cliente',
-correo: 'comprovarasdasd'
-};
+function enviarDatos(nombre, correo) {
+  const dataToSend = {
+    nombre,
+    correo
+  };
 
-axios.post('http://localhost:3000/cliente', dataToSend)
-.then(response => {
-    console.log(response.data);
-})
-.catch(error => {
-    console.error(error);
+  axios.post('http://localhost:3000/cliente', dataToSend)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+document.getElementById('enviar').addEventListener('click', function(event) {
+  const nombre = document.getElementById('nombre').value;
+  const correo = document.getElementById('correo').value;
+
+  enviarDatos(nombre, correo);
 });
